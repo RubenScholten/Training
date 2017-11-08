40d4271fe586c77f43219f380ee3e20236273c5f
RubenScholten <rubenscholten1@gmail.com>
2017-11-08 08:21:02 +0100

    add packages

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.PrintIn(stringutil.Reverse("!oG , elloH"))
+	fmt.PrintIn(stringutil.Myname)
+}
